84b6d5ec763d9ad6c1ee6348e757467f599b6efe
Vitalii Klymov <klimov09032000@gmail.com>
2021-06-17 16:50:41 +0300

    Completed script for nexus instance

--- scripts/create_instance.sh
+++ scripts/create_instance.sh
@@ -18,17 +18,23 @@ MTU="1500"
 
 RESERVE_EXTERNAL_IP_NAME1="external-ip-nginx"
 RESERVE_INTERNAL_IP_NAME1="internal-ip-nginx"
+RESERVE_EXTERNAL_IP_NAME2="external-ip-nexus"
+RESERVE_INTERNAL_IP_NAME2="internal-ip-nexus"
+
 
 FIREWALL_RULE_SSH_HTTP="ssh-http-rule"
+FIREWALL_RULE_NEXUS="nexus-rule"
 FIREWALL_RULE_SSH_HTTP_PORTS="tcp:22,tcp:80,tcp:443,icmp"
+FIREWALL_RULE_NEXUS_PORTS="tcp:8081"
 FIREWALL_RULE_FROM_NETWORK="rule-anywhere"
 FIREWALL_RULE_FOR_JENKINS="jenkins-master-ports"
 
 INSTANCE_NAME1="jenkins-nginx-instance"
 INSTANCE_NAME2="nexus-instance"
+
 MACHINE_TYPE=e2-medium
 HOSTNAME1="jenkins-nginx.asxan.ml"
-HOSTNAME2="je"
+HOSTNAME2="nexus.asxan.ml"
 COUNT=1
 OWNER="vitalii-klymov"
 
@@ -89,20 +95,37 @@ SSH_KEY="/Users/vklymov/.ssh/gcloud_key.pub"
 # --description="It is firewall rule which allow all ports connections from anywhere in the network"
 
 
+gcloud compute firewall-rules create  $FIREWALL_RULE_NEXUS \
+--network=$NETWORK_NAME \
+--action=ALLOW \
+--rules $FIREWALL_RULE_NEXUS_PORTS \
+--source-ranges=$ALL_RANGE \
+--description="The firewall rule for nexus (8081,443)"
+
 # gcloud compute project-info add-metadata \
 # --metadata-from-file \
 # ssh-keys=$SSH_KEY
 
 
-# gcloud compute addresses create $RESERVE_EXTERNAL_IP_NAME \
+# gcloud compute addresses create $RESERVE_EXTERNAL_IP_NAME1 \
 # --description="It is external ip address for nginx instance" \
 # --region=$CLOUDSDK_COMPUTE_REGION 
 
 
-# gcloud compute addresses create $RESERVE_INTERNAL_IP_NAME \
+# gcloud compute addresses create $RESERVE_INTERNAL_IP_NAME1 \
 # --description="It is internak ip address for nginx instance" \
 # --region=$CLOUDSDK_COMPUTE_REGION \
 # --subnet=$PUBLIC_SUBNET_NAME 
+
+gcloud compute addresses create $RESERVE_EXTERNAL_IP_NAME2 \
+--description="It is external ip address for nginx instance" \
+--region=$CLOUDSDK_COMPUTE_REGION 
+
+
+gcloud compute addresses create $RESERVE_INTERNAL_IP_NAME2 \
+--description="It is internak ip address for nginx instance" \
+--region=$CLOUDSDK_COMPUTE_REGION \
+--subnet=$PUBLIC_SUBNET_NAME 
 
 
 gcloud compute instances create $INSTANCE_NAME1 \
@@ -116,5 +139,19 @@ gcloud compute instances create $INSTANCE_NAME1 \
 --image=$IMAGE_TYPE \
 --zone=$AVAILABILITY_ZONE_A \
 --tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_FROM_NETWORK,$FIREWALL_RULE_FOR_JENKINS \
---network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME1:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME1 \
---metadata-from-file=startup-script=/Users/vklymov/Codes/internship-tasks/provision/jenkins_nginx_provision.sh
+--network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME1:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME1
+
+
+
+gcloud compute instances create $INSTANCE_NAME2 \
+--hostname=$HOSTNAME2 \
+--labels ^:^name=$INSTANCE_NAME2:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
+--machine-type=$MACHINE_TYPE \
+--boot-disk-device-name=$BOOT_DISK_NAME \
+--boot-disk-type=$BOOT_DISK_TYPE  \
+--boot-disk-size=$BOOT_DISK_SIZE \
+--image-project=$IMAGE_PROJECT \
+--image=$IMAGE_TYPE \
+--zone=$AVAILABILITY_ZONE_A \
+--tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_NEXUS \
+--network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME2:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME2
