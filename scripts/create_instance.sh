504677186fdc8712ff51ff671a34f606b35445e5
Vitalii Klymov <klimov09032000@gmail.com>
2021-07-02 17:40:21 +0300

    Roles: jenkins, nginx, docker_install, plugins were added to ansible template

--- scripts/create_instance.sh
+++ scripts/create_instance.sh
@@ -34,6 +34,7 @@ FIREWALL_RULE_SSH_HTTP_PORTS="tcp:22,tcp:80,tcp:443,icmp"
 FIREWALL_RULE_NEXUS_PORTS="tcp:8081"
 FIREWALL_RULE_FROM_NETWORK="rule-anywhere"
 FIREWALL_RULE_FOR_JENKINS="jenkins-master-ports"
+FIREWALL_RULE_FOR_JENKINS_PORTS="tcp:8080,tcp:8081,tcp:465,tcp:50000"
 
 INSTANCE_NAME1="jenkins-nginx-instance"
 INSTANCE_NAME2="nexus-instance"
@@ -56,7 +57,7 @@ IMAGE_TYPE="centos-7-v20200403"
 IMAGE_PROJECT="centos-cloud"
 
 
-SSH_KEY="/Users/vklymov/.ssh/gcloud_key.pub"
+SSH_KEY="/Users/vklymov/.ssh/gcloud_gridkey.pub"
 
 
 MANAGED_ZONE="asxan.ml."
@@ -67,41 +68,41 @@ DNS_NAME_2="$HOSTNAME2."
 DNS_NAME_3="$HOSTNAME3."
 DNS_NAME_4="$HOSTNAME4."
 
-#-------------------------------------------------------------#
+# #-------------------------------------------------------------#
 
-# gcloud compute networks create $NETWORK_NAME \
-# --bgp-routing-mode=regional --mtu=$MTU --subnet-mode=custom \
-# --description="This network is for hosting all infrastruction"
-
-
-# gcloud compute networks subnets create $PUBLIC_SUBNET_NAME \ 
-# --network=$NETWORK_NAME \
-# --range=$PUBLIC_RANGE \
-# --region=$CLOUDSDK_COMPUTE_REGION \
-# --description="It is  public subnet" 
+# # gcloud compute networks create $NETWORK_NAME \
+# # --bgp-routing-mode=regional --mtu=$MTU --subnet-mode=custom \
+# # --description="This network is for hosting all infrastruction"
 
 
-# gcloud compute networks subnets create $PRIVATE_SUBNET_NAME \
-# --network=$NETWORK_NAME \
-# --range=$PRIVATE_RANGE \
-# --region=$CLOUDSDK_COMPUTE_REGION \
-# --enable-private-ip-google-access \
-# --description="It is private subnet"
+# # gcloud compute networks subnets create $PUBLIC_SUBNET_NAME \
+# # --network=$NETWORK_NAME \
+# # --range=$PUBLIC_RANGE \
+# # --region=$CLOUDSDK_COMPUTE_REGION \
+# # --description="It is  public subnet" 
 
 
-# gcloud compute routers create $ROUTER_NAME \
-# --network=$NETWORK_NAME \
-# --region=$CLOUDSDK_COMPUTE_REGION \
-# --description="It is cloud router which connected to custom network"
+# # gcloud compute networks subnets create $PRIVATE_SUBNET_NAME \
+# # --network=$NETWORK_NAME \
+# # --range=$PRIVATE_RANGE \
+# # --region=$CLOUDSDK_COMPUTE_REGION \
+# # --enable-private-ip-google-access \
+# # --description="It is private subnet"
 
 
-# gcloud compute routers nats create $NAT_GATEWAY_NAME \
-# --router=$ROUTER_NAME \
-# --region=$CLOUDSDK_COMPUTE_REGION \
-# --nat-custom-subnet-ip-ranges="$PRIVATE_SUBNET_NAME" \
-# --auto-allocate-nat-external-ips
+# # gcloud compute routers create $ROUTER_NAME \
+# # --network=$NETWORK_NAME \
+# # --region=$CLOUDSDK_COMPUTE_REGION \
+# # --description="It is cloud router which connected to custom network"
 
-#-------------------------------------------------------------#
+
+# # gcloud compute routers nats create $NAT_GATEWAY_NAME \
+# # --router=$ROUTER_NAME \
+# # --region=$CLOUDSDK_COMPUTE_REGION \
+# # --nat-custom-subnet-ip-ranges="$PRIVATE_SUBNET_NAME" \
+# # --auto-allocate-nat-external-ips
+
+# #-------------------------------------------------------------#
 
 # gcloud compute firewall-rules create  $FIREWALL_RULE_SSH_HTTP \
 # --network=$NETWORK_NAME \
@@ -125,13 +126,20 @@ DNS_NAME_4="$HOSTNAME4."
 # --source-ranges=$ALL_RANGE \
 # --description="The firewall rule for nexus (8081,443)"
 
-
+gcloud compute firewall-rules create $FIREWALL_RULE_FOR_JENKINS \
+--network=$NETWORK_NAME \
+--action=ALLOW  \
+--direction=INGRESS \
+--rules=$FIREWALL_RULE_FOR_JENKINS_PORTS \
+--source-ranges=$ALL_RANGE  \
+--target-tags=$FIREWALL_RULE_FOR_JENKINS \
+--description="The firewall rule for jenkins(8080,8081,50000,465)"
 
 # gcloud compute project-info add-metadata \
 # --metadata-from-file \
 # ssh-keys=$SSH_KEY
 
-#-------------------------------------------------------------#
+# #-------------------------------------------------------------#
 
 # gcloud compute addresses create $RESERVE_EXTERNAL_IP_NAME1 \
 # --description="It is external ip address for nginx instance" \
@@ -177,32 +185,32 @@ DNS_NAME_4="$HOSTNAME4."
 
 #-------------------------------------------------------------#
 
-# gcloud compute instances create $INSTANCE_NAME1 \
-# --hostname=$HOSTNAME1 \
-# --labels ^:^name=$INSTANCE_NAME1:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
-# --machine-type=$MACHINE_TYPE \
-# --boot-disk-device-name=$BOOT_DISK_NAME \
-# --boot-disk-type=$BOOT_DISK_TYPE  \
-# --boot-disk-size=$BOOT_DISK_SIZE \
-# --image-project=$IMAGE_PROJECT \
-# --image=$IMAGE_TYPE \
-# --zone=$AVAILABILITY_ZONE_A \
-# --tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_FROM_NETWORK,$FIREWALL_RULE_FOR_JENKINS \
-# --network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME1:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME1
+gcloud compute instances create $INSTANCE_NAME1 \
+--hostname=$HOSTNAME1 \
+--labels ^:^name=$INSTANCE_NAME1:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
+--machine-type=$MACHINE_TYPE \
+--boot-disk-device-name=$BOOT_DISK_NAME \
+--boot-disk-type=$BOOT_DISK_TYPE  \
+--boot-disk-size=$BOOT_DISK_SIZE \
+--image-project=$IMAGE_PROJECT \
+--image=$IMAGE_TYPE \
+--zone=$AVAILABILITY_ZONE_A \
+--tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_FROM_NETWORK,$FIREWALL_RULE_FOR_JENKINS \
+--network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME1:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME1
 
 
-# gcloud compute instances create $INSTANCE_NAME2 \
-# --hostname=$HOSTNAME2 \
-# --labels ^:^name=$INSTANCE_NAME2:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
-# --machine-type=$MACHINE_TYPE \
-# --boot-disk-device-name=$BOOT_DISK_NAME \
-# --boot-disk-type=$BOOT_DISK_TYPE  \
-# --boot-disk-size=$BOOT_DISK_SIZE \
-# --image-project=$IMAGE_PROJECT \
-# --image=$IMAGE_TYPE \
-# --zone=$AVAILABILITY_ZONE_A \
-# --tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_NEXUS \
-# --network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME2:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME2
+gcloud compute instances create $INSTANCE_NAME2 \
+--hostname=$HOSTNAME2 \
+--labels ^:^name=$INSTANCE_NAME2:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
+--machine-type=$MACHINE_TYPE \
+--boot-disk-device-name=$BOOT_DISK_NAME \
+--boot-disk-type=$BOOT_DISK_TYPE  \
+--boot-disk-size=$BOOT_DISK_SIZE \
+--image-project=$IMAGE_PROJECT \
+--image=$IMAGE_TYPE \
+--zone=$AVAILABILITY_ZONE_A \
+--tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_NEXUS \
+--network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME2:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME2
 
 
 gcloud compute instances create $INSTANCE_NAME3 \
@@ -219,18 +227,18 @@ gcloud compute instances create $INSTANCE_NAME3 \
 --network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME3:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME3
 
 
-# gcloud compute instances create $INSTANCE_NAME4 \
-# --hostname=$HOSTNAME4 \
-# --labels ^:^name=$INSTANCE_NAME4:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
-# --machine-type=$MACHINE_TYPE \
-# --boot-disk-device-name=$BOOT_DISK_NAME \
-# --boot-disk-type=$BOOT_DISK_TYPE  \
-# --boot-disk-size=$BOOT_DISK_SIZE \
-# --image-project=$IMAGE_PROJECT \
-# --image=$IMAGE_TYPE \
-# --zone=$AVAILABILITY_ZONE_A \
-# --tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_FROM_NETWORK \
-# --network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME4:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME4
+gcloud compute instances create $INSTANCE_NAME4 \
+--hostname=$HOSTNAME4 \
+--labels ^:^name=$INSTANCE_NAME4:owner=$OWNER:subnet=$PUBLIC_SUBNET_NAME \
+--machine-type=$MACHINE_TYPE \
+--boot-disk-device-name=$BOOT_DISK_NAME \
+--boot-disk-type=$BOOT_DISK_TYPE  \
+--boot-disk-size=$BOOT_DISK_SIZE \
+--image-project=$IMAGE_PROJECT \
+--image=$IMAGE_TYPE \
+--zone=$AVAILABILITY_ZONE_A \
+--tags=$FIREWALL_RULE_SSH_HTTP,$FIREWALL_RULE_FROM_NETWORK \
+--network-interface ^:^address=$RESERVE_EXTERNAL_IP_NAME4:network=$NETWORK_NAME:subnet=$PUBLIC_SUBNET_NAME:private-network-ip=$RESERVE_INTERNAL_IP_NAME4
 
 #-------------------------------------------------------------#
 
